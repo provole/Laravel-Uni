8612136ae753d22c588c4fc32de0decf99d78a9c
Volen Angelov <volen.angelov@gmail.com>
2016-03-27 19:15:08 +0100

    Removing the unneeded ul tag

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,12 +9,12 @@
                         <li><a href="/auth/login">Login</a></li>
                         <li><a href="/auth/register">Register</a></li>
                     @else
-                        <li class="dropdown">
+                        
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->first_name . ' ' . Auth::user()->last_name }} <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
+                            <ul>
                                 <li><a href="/auth/logout">Logout</a></li>
                             </ul>
-                        </li>
+                     
                     @endif
 </ul>
 @include('layouts.menu')

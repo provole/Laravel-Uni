9c17fac836f89f6dbbdee33f7327c4fb7caeadc4
Volen Angelov <volen.angelov@gmail.com>
2016-03-28 13:41:55 +0100

    Adding the menu and nav to contact edit and show pages

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -2,8 +2,24 @@
 @section('title')
 Edit {{$product->name}}
 @stop
-
+<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}"/>
 @section('body')
+<div class = "menu">
+<ul>
+	@if (Auth::guest())
+                        <li><a href="/auth/login">Login</a></li>
+                        <li><a href="/auth/register">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->first_name . ' ' . Auth::user()->last_name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="/auth/logout">Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+</ul>
+	</div>
+@include('layouts.menu')
 	{!!Form::model($product, [
 		'method' => 'patch',
 		'route' => ['product.update', $product->id]

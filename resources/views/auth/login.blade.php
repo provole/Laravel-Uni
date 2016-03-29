95c8cace4e1aee7cb8f9b6f7cef7777a9c6249d5
Volen Angelov <volen.angelov@gmail.com>
2016-03-29 12:53:33 +0100

    Fixing the login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,25 @@
+@extends('layouts.layout')
+@section('title')
+	All products
+@stop
+<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}"/>  
+@section('body')
+<div class = "menu">
+<ul>
+	@if (Auth::guest())
+                        <li><a href="/auth/login">Login</a></li>
+                        <li><a href="/auth/register">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->first_name . ' ' . Auth::user()->last_name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="/auth/logout">Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+</ul>
+	</div>
+@include('layouts.menu')
 <form method="POST" action="/auth/login">
     {!! csrf_field() !!}
 
@@ -19,3 +41,4 @@
         <button type="submit">Login</button>
     </div>
 </form>
+@stop

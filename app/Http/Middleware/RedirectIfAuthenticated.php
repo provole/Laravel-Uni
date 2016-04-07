b724bb5c48a1867fe6a56a34e0880578890bcc2f
Volen Angelov <volen.angelov@gmail.com>
2016-04-07 13:25:20 +0100

    in process of fixing the redirect when logout

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -35,7 +35,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next)
     {
         if ($this->auth->check()) { //Auth::check
-            return redirect('/home');
+            return redirect('/produkt');
         }
 
         return $next($request);
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@doLogout');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');

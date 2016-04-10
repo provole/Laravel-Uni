05a1908c1f52b882bf6c5627f94eb719ef0f222f
Volen Angelov <volen.angelov@gmail.com>
2016-04-10 17:30:55 +0100

    Fixing an error in the about page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,22 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
-use DB;
+
 class PagesController extends Controller
 {
     public function getIndex(){
         return view('welcome');
     }
 
-    public function getAbout(){
+   public function getAbout(){
+
 		
-      
-			
-	return view('pages.about')
-        	
-			
-	
-    }
+      			
+ 	return view('pages.about');
+         	
+ 		
+ 
+      }
 
     public function getContact(){
         return view('pages.contact');

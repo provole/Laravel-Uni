e87e24c599fcfb37c007d5ef4626151982504a82
Volen Angelov <volen.angelov@gmail.com>
2016-04-11 12:40:40 +0100

    Implemented a to-do task

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,49 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
+use App\Task;
+use Illuminate\Http\Request;
+
+
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/tasks', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
+
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/tasks');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/tasks');
+    });
+
 
 
 
